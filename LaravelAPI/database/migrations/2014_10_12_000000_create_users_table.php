e902fc0795c4c43f85ba5b6884f9a4d86c154d04
Marcelo Rios <chelo38@gmail.com>
2020-12-26 18:16:44 -0400

    Passport, part 1

--- LaravelAPI/database/migrations/2014_10_12_000000_create_users_table.php
+++ LaravelAPI/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('photo');
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();         
         });
     }
 
