81b56938a4ff106a2f801acb0de90c1ff03e4c27
Marcelo Rios <chelo38@gmail.com>
2020-12-27 14:19:10 -0400

    Added network class

--- flutter_client/lib/network_utils/api.dart
+++ flutter_client/lib/network_utils/api.dart
@@ -1,1 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
+class Network {
+  final String _url = 'http://localhost:8000/api/v1';
+  var token;
+
+  _getToken() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    token = jsonDecode(localStorage.getString('token'))['token'];
+  }
+
+  authData(data, apiUrl) async {
+    //Handles post requests (log-in/register)
+    var fullUrl = _url + apiUrl;
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
+  }
+
+  getData(apiUrl) async {
+    //Handles get request (including log-out)
+    var fullUrl = _url + apiUrl;
+    await _getToken();
+    return await http.get(fullUrl, headers: _setHeaders());
+  }
+
+  _setHeaders() => {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token'
+      };
+}
