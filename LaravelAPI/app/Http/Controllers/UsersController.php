cbbfbccfe5149ba18b728bbdd87bb2a5860726bc
Marcelo Rios <chelo38@gmail.com>
2020-12-28 19:51:41 -0400

    Modified UsersController's register function to accept and store user's photos

--- LaravelAPI/app/Http/Controllers/UsersController.php
+++ LaravelAPI/app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Validator;
 use App\Models\User;
 use Auth;
 
+
 class UsersController extends Controller
 {
     public function login()
@@ -35,29 +36,63 @@ class UsersController extends Controller
      */
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'fname' => 'required',
-            'lname' => 'required',
-            'phone' => 'required|unique:users|regex:/(0)[0-9]{10}/',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-        ]);
-        if ($validator->fails()) {
-          return response()->json([
-            'success' => false,
-            'message' => $validator->errors(),
-          ], 401);
-        }
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] = $user->createToken('appToken')->accessToken;
+      //return $request->file('photo');
+
+      //Validation of all fields
+      $validator = Validator::make($request->all(), [
+          'fname' => 'required',
+          'lname' => 'required',
+          'address' => 'required',
+          'phone' => 'required|unique:users|regex:/(0)[0-9]{10}/',
+          'photo' => 'required',
+          'email' => 'required|email|unique:users',
+          'password' => 'required',
+      ]);
+      if ($validator->fails()) {
         return response()->json([
-          'success' => true,
-          'token' => $success,
-          'user' => $user
+          'success' => false,
+          'message' => $validator->errors(),
+        ], 401);
+      }
+
+      //Validation of photo: Verification of presence
+      if(!$request->hasFile('photo')) {
+        return response()->json([
+          'success' => false,
+          'message' => 'upload_file_not_found',
+        ], 400);
+      }
+
+      //Validation of photo: Verification of file type
+      $allowedfileExtension=['jpg','jpeg','png'];
+      $file = $request->file('photo');
+      $errors = [];
+
+      $extension = $file->getClientOriginalExtension();  
+      $check = in_array($extension,$allowedfileExtension);
+      if(!$check) {
+        return response()->json([
+          'success' => false,
+          'message' => 'invalid_file_format',
+        ], 422);
+      }
+
+      // Store photo
+      $path = $file->store('public/images');
+
+      // Store user
+      $input = $request->all();
+      $input['password'] = bcrypt($input['password']);
+      $input['photo'] = $path;
+      $user = User::create($input);
+      $success['token'] = $user->createToken('appToken')->accessToken;
+      return response()->json([
+        'success' => true,
+        'token' => $success,
+        'user' => $user
       ]);
     }
+
 
     public function logout(Request $res)
     {
@@ -75,5 +110,5 @@ class UsersController extends Controller
           'message' => 'Unable to Logout'
         ]);
       }
-     }    
-}
+    }    
+};
--- README.md
+++ README.md
@@ -8,3 +8,15 @@ https://medium.com/@godilite/laravel-6-api-authentication-with-laravel-passport-
 
 3. Postman Tutorial: How to use Postman Tool for API Testing
 https://www.guru99.com/postman-tutorial.html
+
+4. Laravel 8 Multiple Image Upload Via API
+https://www.tutsmake.com/laravel-8-multiple-image-upload-via-api/
+
+5. A Complete Guide to Laravel Storage
+https://www.larashout.com/a-complete-guide-to-laravel-storage
+
+6. Using the Camera on Flutter
+https://www.raywenderlich.com/4333657-using-the-camera-on-flutter
+
+7. Flutter File Uploads
+https://fireship.io/lessons/flutter-file-uploads-cloud-storage/
